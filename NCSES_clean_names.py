34f603f178e8105774b24c31b04a931ac554afdb
Matt VanEseltine <matvan@umich.edu>
2019-07-19 15:20:43 -0400

    Export fields in alphabetical order

--- NCSES_clean_names.py
+++ NCSES_clean_names.py
@@ -204,7 +204,7 @@ def write_output(output_table, output_file, output_fields):
     output_path.parent.mkdir(exist_ok=True)
 
     # Derive the fieldnames from the first row.
-    fields = set(field for row in output_table for field in row)
+    fields = sorted(set(field for row in output_table for field in row))
     assert set(output_fields) <= set(fields), "Not all output fields were created."
 
     with output_path.open("w", newline="", encoding="utf-8") as outfile:
