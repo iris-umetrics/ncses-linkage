50bc7e85e442455955a8f60e7f424ed028a41203
Matt VanEseltine <matvan@umich.edu>
2019-07-19 12:11:23 -0400

    Fix error in nickname_mapping normalization and test

--- test_NCSES_clean_names.py
+++ test_NCSES_clean_names.py
@@ -5,15 +5,23 @@ import pytest
 from NCSES_clean_names import load_nicknames, NICKNAME_FILENAME
 
 nicknames = load_nicknames(NICKNAME_FILENAME)
+all_names = set(nicknames.keys()) ^ set(nicknames.values())
 
 
 def test_nicknames_no_chained_key_values():
-    assert not set(nicknames.keys()) & set(nicknames.values())
+    intersect = set(nicknames.keys()) & set(nicknames.values())
+    assert not intersect
 
 
 def test_nicknames_all_fully_normalized():
-    all_values = set(nicknames.keys()) ^ set(nicknames.values())
-    assert all(re.match(r"[a-z]+", x) for x in all_values)
+    assert all(re.match(r"^[a-z]+$", name) for name in all_names)
+
+
+# Long and slow -- for diagnosis on a problem. Remove SKIP_ from function name to run.
+@pytest.mark.parametrize("name", all_names)
+def SKIP_test_slowly_nicknames_all_fully_normalized(name):
+    # Remove SKIP_ from function name to run.
+    assert re.match(r"^[a-z]+$", name)
 
 
 from NCSES_clean_names import clean_integer
